93f9b880d10b419050833634a1a1a3e694c25f76
mamoud <mamoud.antacid895@passmail.com>
2025-02-13 23:45:24 +0100

    fix: replaced ts @ path by node subpath

--- apps/backend/src/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@/*';
-declare module '@config/*';
-declare module '@controllers/*';
-declare module '@lib/*';
-declare module '@middlewares/*';
-declare module '@routes/*';
-declare module '@services/*'; 

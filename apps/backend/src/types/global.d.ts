e42f09f94327bf4353b09e256a81fb09717de515
mamoud <mamoud.antacid895@passmail.com>
2025-02-13 23:42:30 +0100

    feat: initial commit for backend, added file structure and dependancies

---
+++ apps/backend/src/types/global.d.ts
@@ -0,0 +1,7 @@
+declare module '@/*';
+declare module '@config/*';
+declare module '@controllers/*';
+declare module '@lib/*';
+declare module '@middlewares/*';
+declare module '@routes/*';
+declare module '@services/*'; 
---
+++ apps/backend/src/types/package.d.ts
@@ -0,0 +1,14 @@
+declare module "fastify" {
+  export * from "@fastify/fastify";
+  export { default } from "@fastify/fastify";
+}
+
+declare module "@fastify/jwt" {
+  export * from "@fastify/jwt";
+  export { default } from "@fastify/jwt";
+}
+
+declare module "@fastify/websocket" {
+  export * from "@fastify/websocket";
+  export { default } from "@fastify/websocket";
+} 

bcc57a211a656a94147b5aa088674a3d79104f9e
mamoud <mamoud.antacid895@passmail.com>
2025-02-27 15:28:43 +0100

    feat(backend): registered tournament controller to tournament routes

--- apps/backend/src/routes/tournament.routes.ts
+++ apps/backend/src/routes/tournament.routes.ts
@@ -1,11 +1,14 @@
 import { FastifyInstance } from "fastify";
+import { TournamentController } from "#controllers/tournament.controller";
+import { TournamentService } from "#services/tournament.service";
 
 async function tournamentRoutes(app: FastifyInstance) {
+  const tournamentService = new TournamentService(app);
+  const tournamentController = new TournamentController(tournamentService);
+
   app.addHook("onRequest", app.authenticate);
 
-  app.get("/", async (request, reply) => {
-    console.log("get tournaments");
-  });
+  app.get("/", tournamentController.getTournaments);
 
   app.post(
     "/create",
@@ -24,26 +27,14 @@ async function tournamentRoutes(app: FastifyInstance) {
         },
       },
     },
-    async (request, reply) => {
-      console.log("create tournament");
-    }
+    tournamentController.createTournament
   );
 
-  app.get("/:id", async (request, reply) => {
-    console.log("get tournament");
-  });
+  app.get("/:id", tournamentController.getTournament);
 
-  app.post("/join/:id", async (request, reply) => {
-    console.log("join tournament");
-  });
+  app.post("/join/:id", tournamentController.joinTournament);
 
-  app.post("/start/:id", async (request, reply) => {
-    console.log("start tournament");
-  });
-
-  app.post("/finish/:id", async (request, reply) => {
-    console.log("finish tournament");
-  });
+  app.post("/start/:id", tournamentController.startTournament);
 }
 
 export default tournamentRoutes;
